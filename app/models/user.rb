5bdc1293653fbe10fca73501e1f03dec76c14b8a
teruteru-bose <takemoto9570@gmail.com>
2022-06-17 20:08:40 +0900

    新規登録・ユーザー登録・本人情報確認・トップページ

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  validates :nickname,        presence: true
+
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+  validates :password,
+  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'を半角英数字混合で入力してください' }
+
+  validates :first_name,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'を全角（漢字・ひらがな・カタカナ）で入力してください' }
+
+  validates :last_name,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'を全角（漢字・ひらがな・カタカナ）で入力してください' }
+  
+  validates :first_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/, message: 'を全角（カタカナ）で入力してください' }
+
+  validates :last_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/, message: 'を全角（カタカナ）で入力してください' }
+
+  validates :birthday,        presence: true
+
+end
