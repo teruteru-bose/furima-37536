43d17fbc6887c13ea1c832ef6999454735f18b60
teruteru-bose <takemoto9570@gmail.com>
2022-06-23 21:09:08 +0900

    商品詳細表示機能（Rubocopにて自動修正済）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,10 +18,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id,
-                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+                                :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
