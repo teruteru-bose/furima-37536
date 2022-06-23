920c1bb6224e8620ad1a850ad726751aa67d28b9
teruteru-bose <takemoto9570@gmail.com>
2022-06-23 19:35:07 +0900

    商品一覧表示機能（Rubocopにて自動修正済）

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,9 @@ class Item < ApplicationRecord
   validates :delivery_fee_id, numericality: { other_than: 1, message: 'を選択してください' }
   validates :prefecture_id,   numericality: { other_than: 1, message: 'を選択してください' }
   validates :shipping_day_id, numericality: { other_than: 1, message: 'を選択してください' }
-  validates :price,           numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   belongs_to :user
+  has_one :purchase
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,5 @@ class User < ApplicationRecord
   validates :birthday, presence: true
 
   has_many :items
+  has_many :purchases
 end
